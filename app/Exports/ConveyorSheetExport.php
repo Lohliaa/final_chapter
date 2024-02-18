b9cd85e03dc88d51369a17f49a005755a7d9589d
Lohliaa <LiaAufarrahman09@gmail.com>
2024-02-18 13:19:25 +0700

    report perconveyor

--- app/Exports/ConveyorSheetExport.php
+++ app/Exports/ConveyorSheetExport.php
@@ -7,8 +7,11 @@ use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use App\Exports\ReportCVExport;
 use App\Utilities\ExportUtilities;
+use Illuminate\Support\Facades\Auth;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class ConveyorSheetExport implements FromCollection, WithHeadings
+class ConveyorSheetExport implements FromCollection, WithHeadings, ShouldAutoSize
+
 {
     protected $conveyorData;
     protected $conveyorType;
@@ -25,18 +28,21 @@ class ConveyorSheetExport implements FromCollection, WithHeadings
     public function collection()
     {
         $rows = [];
+        $user = Auth::id();
 
         foreach ($this->conveyorData as $conveyor) {
             $cl_sum_proses = DB::table('proses')
                 ->where('conveyor', $conveyor->conveyor)
                 ->where('kind_size_color', $conveyor->kind_size_color)
                 ->where('cust_part_no', $conveyor->cust_part_no)
+                ->where('user_id', $user)
                 ->sum(DB::raw('(cl * total_qty) / 1000'));
 
             $cl_sum_proses_fa1a = DB::table('proses_fa_1a')
                 ->where('conveyor', $conveyor->conveyor)
                 ->where('kind_size_color', $conveyor->kind_size_color)
                 ->where('cust_part_no', $conveyor->cust_part_no)
+                ->where('user_id', $user)
                 ->sum(DB::raw('(cl * total_qty) / 1000'));
 
             $cl_sum_combined = $cl_sum_proses + $cl_sum_proses_fa1a;
@@ -54,92 +60,115 @@ class ConveyorSheetExport implements FromCollection, WithHeadings
             'term_a', 'acca1', 'acca2', 'tubea',
         ];
 
-        $combinedResults = [];
+        $results = [];
 
-        foreach ($this->conveyorData as $conveyor) {
-            $combinedResults[$conveyor->conveyor] = [];
-            foreach ($groups as $group) {
-                $prosesData = DB::table('proses')
-                    ->where('conveyor', $conveyor->conveyor)
-                    ->select($groups)  
-                    ->groupBy($groups) 
-                    ->get();
+        foreach ($groups as $group) {
+            $data_fa = DB::table('proses')
+                ->where('conveyor', $conveyor->conveyor)
+                ->select($group)
+                ->groupBy($group)
+                ->where('user_id', $user)
+                ->get();
 
-                $prosesFa1aData = DB::table('proses_fa_1a')
-                    ->where('conveyor', $conveyor->conveyor)
-                    ->select($groups)  
-                    ->groupBy($groups) 
-                    ->get();
+            $data_pa = DB::table('proses_fa_1a')
+                ->where('conveyor', $conveyor->conveyor)
+                ->select($group)
+                ->groupBy($group)
+                ->where('user_id', $user)
+                ->get();
+            $data = $data_fa->merge($data_pa);
+            foreach ($data as $item) {
+                $key = $item->{$group};
 
-                $combinedData = $prosesData->concat($prosesFa1aData);
-                $uniqueResultsByConveyor = [];
+                $numbers = explode('-', $key);
 
-                foreach ($combinedData as $item) {
-                    foreach ($groups as $columnName) {
-                        $key = $item->{$columnName};
-                
-                        $numbers = explode('-', $key);
-                
-                        $isNumeric = true;
-                        foreach ($numbers as $number) {
-                            if (!is_numeric($number)) {
-                                $isNumeric = false;
-                                break;
-                            }
-                        }
-                        $containsHyphen = strpos($key, '-') !== false;
-                        $containsLetter = preg_match('/[a-zA-Z]/', $key);
-                
-                        if (($isNumeric && $containsHyphen) || ($containsHyphen && $containsLetter)) {
-                            $qtySumProses = DB::table('proses')
-                                ->where('conveyor', $conveyor->conveyor)
-                                ->where($columnName, $key)
-                                ->sum('total_qty');
-                
-                            $qtySumProsesFa1a = DB::table('proses_fa_1a')
-                                ->where('conveyor', $conveyor->conveyor)
-                                ->where($columnName, $key)
-                                ->sum('total_qty');
-                
-                            $qtySumCombined = $qtySumProses + $qtySumProsesFa1a;
-                
-                            $itemList = DB::table('item_list')
-                                ->where('part_no', 'LIKE', '%' . $key . '%')
-                                ->first();
-                
-                            $material = $key;
-                            $buppin = $key;
-                
-                            if ($itemList) {
-                                $formattedPartNo = $this->convertMaterialToPartNo($itemList->part_no);
-                                $buppin = $itemList->cust_pno;
-                                if ($formattedPartNo == $material) {
-                                    $buppin = $itemList->cust_pno;
-                                }
-                            }
-                
-                            $combinedKey = $material . '-' . $buppin;
-                
-                            if (!isset($uniqueResultsByConveyor[$conveyor->conveyor][$combinedKey])) {
-                                $uniqueResultsByConveyor[$conveyor->conveyor][$combinedKey] = [
-                                    'conveyor' => $conveyor->conveyor,
-                                    'material' => $material,
-                                    'buppin' => $buppin,
-                                    'qty_sum_combined' => $qtySumCombined,
-                                ];
-                                $finalResults[] = $uniqueResultsByConveyor[$conveyor->conveyor][$combinedKey];
-                            } else {
-                                $uniqueResultsByConveyor[$conveyor->conveyor][$combinedKey]['qty_sum_combined'] += $qtySumCombined;
-                            }
-                        }
+                $isNumeric = true;
+                foreach ($numbers as $number) {
+                    if (!is_numeric($number)) {
+                        $isNumeric = false;
+                        break;
                     }
                 }
+                $containsHyphen = strpos($key, '-') !== false;
+                $containsLetter = preg_match('/[a-zA-Z]/', $key);
 
-                $rowsAndCombined = array_merge($rows, $finalResults);
+                if ($isNumeric) {
+                    $qtySumProses = DB::table('proses')
+                        ->where('user_id', $user)
+                        ->where('conveyor', $conveyor->conveyor)
+                        ->where($group, $key)
+                        ->sum('total_qty');
 
-                return collect($rowsAndCombined);
+                    $qtySumProsesFa1a = DB::table('proses_fa_1a')
+                        ->where('user_id', $user)
+                        ->where('conveyor', $conveyor->conveyor)
+                        ->where($group, $key)
+                        ->sum('total_qty');
+
+                    $qtySumCombined = $qtySumProses + $qtySumProsesFa1a;
+
+                    if (!isset($results[$key])) {
+                        $results[$key] = [
+                            'conveyor' => $conveyor->conveyor,
+                            'material' => $key,
+                            'buppin' => $key,
+                            'qty_sum_combined' => 0,
+                        ];
+                    }
+
+                    $results[$key]['qty_sum_combined'] += $qtySumCombined;
+                } elseif ($containsHyphen && $containsLetter) {
+                    $qtySumProses = DB::table('proses')
+                        ->where('user_id', $user)
+                        ->where('conveyor', $conveyor->conveyor)
+                        ->where($group, $key)
+                        ->sum('total_qty');
+
+                    $qtySumProsesFa1a = DB::table('proses_fa_1a')
+                        ->where('user_id', $user)
+                        ->where('conveyor', $conveyor->conveyor)
+                        ->where($group, $key)
+                        ->sum('total_qty');
+
+                    $qtySumCombined = $qtySumProses + $qtySumProsesFa1a;
+
+                    $itemList = DB::table('item_list')
+                        ->where('part_no', 'LIKE', '%' . $key . '%')
+                        ->where('user_id', $user)
+                        ->first();
+
+                    $material = $key;
+                    $buppin = $key;
+
+                    if ($itemList) {
+                        $exportUtilities = new ExportUtilities();
+                        $formattedPartNo = $exportUtilities->convertMaterialToPartNo($itemList->part_no);
+                        $buppin = $itemList->cust_pno;
+
+                        if ($formattedPartNo == $material) {
+                            $buppin = $itemList->cust_pno;
+                        }
+                    }
+
+                    if (!isset($results[$key])) {
+                        $results[$key] = [
+                            'conveyor' => $conveyor->conveyor,
+                            'material' => $material,
+                            'buppin' => $buppin,
+                            'qty_sum_combined' => 0,
+                        ];
+                    }
+
+                    $results[$key]['qty_sum_combined'] += $qtySumCombined;
+                }
             }
         }
+        $combinedResults = [
+            'rows' => $rows,
+            'results' => $results,
+        ];
+
+        return collect($combinedResults);
     }
 
     public function headings(): array
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -9,7 +9,7 @@
                 <div class="card border-0 shadow rounded">
                     <div class="card-body">
                         <div class="form-row justify-content-center">
-                            <div class="form-group col-6">
+                            <div class="form-group col-4">
                                 <!-- small box -->
                                 <div class="small-box bg-info" style="height: 200px; position: relative; display: flex; justify-content: center; align-items: center;">
                                     <div class="inner">
@@ -18,16 +18,16 @@
                                     <a href="{{ url('export') }}" class="small-box-footer" style="position: absolute; bottom: 0; left: 0; width: 100%; color: #fff; padding: 5px; text-align: center; text-decoration: none;">Download Report <i class="fas fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
-                            {{--  <div class="form-group col-4">
+                            <div class="form-group col-4">
                                 <!-- small box -->
                                 <div class="small-box bg-info" style="height: 200px; position: relative; display: flex; justify-content: center; align-items: center;">
                                     <div class="inner">
-                                        <h3 style="margin: 0;">QTY</h3>
+                                        <h3 style="margin: 0;">QTY CONVEYOR</h3>
                                     </div>
                                     <a href="{{ url('export_cv') }}" class="small-box-footer" style="position: absolute; bottom: 0; left: 0; width: 100%; color: #fff; padding: 5px; text-align: center; text-decoration: none;">Download Report <i class="fas fa-arrow-circle-right"></i></a>
                                 </div>
-                            </div>  --}}
-                            <div class="form-group col-6">
+                            </div>
+                            <div class="form-group col-4">
                                 <!-- small box -->
                                 <div class="small-box bg-info" style="height: 200px; position: relative; display: flex; justify-content: center; align-items: center;">
                                     <div class="inner">
