00f6c69ce718624c7c2adaab6fffa2cf93f54aed
Lohliaa <LiaAufarrahman09@gmail.com>
2024-03-02 21:36:37 +0700

    coba testing login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+context('Login', () => {
+  const loginUser = (email, password) => {
+    cy.get('#email').type(email);
+    cy.get('#password').type(password);
+    cy.get('form').submit();
+  };
+
+  beforeEach(() => {
+    const url = 'http://localhost:8000/home'; // Ganti dengan URL yang sesuai
+    cy.log(`Visiting URL: ${url}`);
+    cy.visit(url);
+  });
+
+  it('Success Login', () => {
+    loginUser("admin@gmail.com", "admin");
+    cy.url().should('include', '/home');
+  });
+}); 
